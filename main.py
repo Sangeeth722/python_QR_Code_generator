e4df65a9345adfcce8a301c78e2efc62f9a142ba
Sangeeth722 <72011368+Sangeeth722@users.noreply.github.com>
2023-08-15 23:35:56 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import qrcode
+url = input("enter yor url")
+features = qrcode.QRCode(version=1,box_size=40,border=3)
+
+features.add_data(url)
+features.make(fit=True)
+genarate_image = features.make_image(fill_color = "Black", back_color = "Whiite")
+genarate_image.save("QR CODE.png")
+
+
